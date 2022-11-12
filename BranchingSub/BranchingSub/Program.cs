196903504b9bbc5013a272cc5ce321642825555a
Eli-gav <108551274+Eli-gav@users.noreply.github.com>
2022-11-11 19:08:38 -0800

    Fixed var 7

--- BranchingSub/BranchingSub/Program.cs
+++ BranchingSub/BranchingSub/Program.cs
@@ -31,16 +31,17 @@ namespace BranchingSub
             Console.WriteLine("Please enter the package length: ");
             int packLength = Convert.ToInt32(Console.ReadLine());
 
-
-            
-            int compar = (packWidth * packHeight * packLength * packWeight)/100;
-
-            if (compar > 50)
+            int allDem = (packWidth + packHeight + packLength);
+            if (allDem > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
                 Console.ReadLine();
                 Environment.Exit(0);
             }
+
+            int compar = (packWidth * packHeight * packLength * packWeight)/100;
+
+            
             
             Console.WriteLine("Your estimated total for shipping this package is: ");
             Console.WriteLine(compar + ".00$" + "  Thank you!");
