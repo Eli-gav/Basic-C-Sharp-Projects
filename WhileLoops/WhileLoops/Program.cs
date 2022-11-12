6dc92f19a62cb3d8f1e414b3a4eaed817756af09
Eli-gav <108551274+Eli-gav@users.noreply.github.com>
2022-11-11 19:43:55 -0800

    While Loop

---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the value of x?");
+            Console.WriteLine("-15 + (-5x) = 0");
+            int num = Convert.ToInt32(Console.ReadLine());
+            bool correctNum = num == -3;
+
+            do
+            {
+                switch (num)
+                {
+                    case 3:
+                        Console.WriteLine("You guessed 3. Close but try again");
+                        Console.WriteLine("Guess a number");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case -3:
+                        Console.WriteLine("You got -3. That is Correct");
+                        correctNum = true;
+                        break;
+                    default:
+                        Console.WriteLine("You're not even close. Try again.");
+                        Console.WriteLine("Guess a number");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+
+            }
+            while (!correctNum);
+            {
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
