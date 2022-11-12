beb3d4b8cfe078299c69f26084ddf40a958c9c1b
Eli-gav <108551274+Eli-gav@users.noreply.github.com>
2022-11-11 16:41:10 -0800

    Branching Submmisson

---
+++ BranchingSub/BranchingSub/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight: ");
+            int packWeight = Convert.ToInt32(Console.ReadLine());
+            if (packWeight >=50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+
+            Console.WriteLine("Please enter the package width: ");
+            int packWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height: ");
+            int packHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length: ");
+            int packLength = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("Your estimated total for shipping this package is: ");
+            int compar = (packWidth * packHeight * packLength * packWeight)/100;
+            Console.WriteLine(compar + ".00$" + "  Thank you!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
