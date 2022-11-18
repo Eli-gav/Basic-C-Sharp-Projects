28c58a4ca8716457d9a97764d9fa236d9732216d
Eli-gav <108551274+Eli-gav@users.noreply.github.com>
2022-11-18 15:11:36 -0800

    ExceptionHandling

---
+++ TestingExceptionHandling/TestingExceptionHandling/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            try
+            {
+                List<int> nums = new List<int>() { 4, 8, 12, 16, 22 };
+                Console.WriteLine("Pick a number");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int i in nums)
+                {
+                    Console.WriteLine(i / userInput);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type in number format instead of word");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please dont divide by zero");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
+
+
+                
+
+               
+              
+                
+
