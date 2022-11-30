b1eaf12e94003d5ad57bf30cefb9b734487f4b10
Eli-gav <108551274+Eli-gav@users.noreply.github.com>
2022-11-29 20:58:27 -0800

    CallingMethods

---
+++ CallingMethodsSubmission/CallingMethodsSubmission/Got.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    public class Got
+    {
+        public Got()
+        {
+            storeNum = 2;
+            //userInput = 4;
+        }
+
+        public int storeNum { get; set; }
+        public int userInput { get; set; }
+    }
+}
---
+++ CallingMethodsSubmission/CallingMethodsSubmission/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Got got = new Got();
+
+            Console.WriteLine("Pick a number to + - or * ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            got.userInput = userInput;
+
+
+            Console.WriteLine("Your number added is " + (AddNumber(got)));
+            Console.WriteLine("Your number subtracted is " + SubNumber(got));
+            Console.WriteLine("Your number multiplied is " + MultNumber(got));
+            Console.ReadLine();
+
+
+
+
+        }
+
+        public static int AddNumber(Got got)
+        {
+            return got.storeNum + got.userInput;
+
+
+
+
+
+        }
+
+        public static int SubNumber(Got got)
+        {
+
+            return got.storeNum - got.userInput;
+
+        }
+
+
+        public static int MultNumber(Got got)
+        {
+
+            return got.storeNum * got.userInput;
+        }
+    }
+}
