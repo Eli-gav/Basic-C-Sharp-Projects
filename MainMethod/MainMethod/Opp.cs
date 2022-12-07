1cf265676c807b7f90c8389c36391b8bdda0c074
Eli-gav <108551274+Eli-gav@users.noreply.github.com>
2022-12-07 05:24:30 -0800

    Main Method

---
+++ MainMethod/MainMethod/Opp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class Opp
+    {
+        public Opp()
+        {
+            one = 555;
+            two = (int)(double)3.4455;
+            three = "Your number is ";
+
+        }
+
+        public static int testOne(Opp opp)
+        {
+            return opp.one + opp.userInput;
+        }
+
+       
+
+        public static int testTwo(Opp opp)
+        {
+            return (int)((int)opp.two * opp.userInput2) ;
+        }
+
+        public static string testThree(Opp opp)
+        {
+            return opp.three +  opp.userInput3;
+        }
+
+ 
+
+
+
+        public int one { get; set; }
+        public int two { get; set; }
+        public string three { get; set; }
+        public int userInput { get; set; }
+        public double userInput2 { get; set; }
+        public string userInput3 { get; set; }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opp opp = new Opp();
+
+            Console.WriteLine("Pick your number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            opp.userInput = userInput;
+            Console.WriteLine("Your number plus 555 is " + (Opp.testOne(opp)));
+
+
+
+
+            Console.WriteLine("Pick a decimal");
+            double userInput2 = Convert.ToDouble(Console.ReadLine());
+            opp.userInput2 = userInput2;
+            Console.WriteLine("Your number multiplied by .4445 is  " + (Opp.testTwo(opp)));
+
+
+            Console.WriteLine("Type in a number by letters ex. Five, Four");
+            string userInput3 = Console.ReadLine();
+            opp.userInput3 = userInput3;
+            
+
+            Console.WriteLine( (Opp.testThree(opp)));
+
+
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
